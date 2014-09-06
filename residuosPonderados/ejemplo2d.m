c717ce2fb1c84d28c1a20e6eeeda856eff16e735
Thejuampi <juanpablo.lescano@gmail.com>
2014-09-06 11:26:56 -0300

    version inicial, sin funcionar

--- residuosPonderados/ejemplo2d.m
+++ residuosPonderados/ejemplo2d.m
@@ -3,3 +3,21 @@
 % d2/dx2)*phi + (d2/dy2)*phi = -2*G*Theta
 % phi(contorno) = 0
 
+syms ax ay x y
+
+
+ax = [1 3 1];
+ay = [1 1 3];
+
+xini = -3;
+xfin = 3;
+yini = -2;
+yfin = 2;
+
+M = 3; % cantidad de funciones de prueba, tamaño de la matriz, etc.
+
+%psi 
+psi = 0; % por la condicion de borde phi(contorno) = 0
+
+%N[m](x): funciones simétricas dada las propiedades del problema
+N = cos(pi*x/6)*cos(pi*x/4);
