e20ba38ecf7fc99e2af4641c64a12415a2cfafe2
Juan Pablo <juanpablo.lescano@gmail.com>
2014-10-11 15:35:39 -0300

    ejemplo 2.6 funcionando con integracion numerica de matlab.

--- residuosPonderados/ejemplo2_6Libro.m
+++ residuosPonderados/ejemplo2_6Libro.m
@@ -1,11 +1,14 @@
 clear all;close all;
-M=3;
+M=4;
 N=@(x,m) x.^m;
-W=N;
+W=@(x,l) x.^l;
 epsilon = 1E-8;
 
-dW = @(x,m) (W(x+epsilon,m)-W(x-epsilon,m))/(2*epsilon); %derivacion numerica Orden²
-dN = @(x,m) (N(x+epsilon,m)-N(x-epsilon,m))/(2*epsilon); %idem dW
+%dW = @(x,m) (W(x+epsilon,m)-W(x-epsilon,m))/(2*epsilon); %derivacion numerica Orden²
+%dN = @(x,m) (N(x+epsilon,m)-N(x-epsilon,m))/(2*epsilon); %idem dW
+
+dW = @(x,m) m.*x.^(m-1);
+dN = dW;
 
 Wat1=@(m) W(1,m);
 
@@ -14,11 +17,26 @@ f = zeros(M,1);
 
 for l = 1:M
     for m = 1:M
-        K(l,m) = integral(@(x) dW(x,l).*dN(x,m) + W(l,x).*N(m,x), 0, 1); %Integracion numerica recomendad por MatLab
+        I1 = integral(@(x) dW(x,l).*dN(x,m), 0, 1);
+        I2 = integral(@(x) W(x,l).*N(x,m), 0, 1);
+        K(l,m) = I1+I2; %Integracion numerica recomendad por MatLab
     end
     f(l) = Wat1(l)*20;
 end
 
 a = K\f;
 
-disp(a);
+phi = @(x) a(1:M).*N(x,1:M)';
+dphi = @(x) a(1:m).*dN(x,1:M)';
+
+x = 0:0.1:1;
+
+phi_q = zeros(size(x));
+dphi_q = zeros(size(x));
+
+for i = 1:length(x)
+    phi_q(i) = sum(phi(x(i)));
+    dphi_q(i) = sum(dphi(x(i)));
+end
+
+plot(x,phi_q);
