e30c953f2b6ee8717d1722423d04213b5808d583
Thejuampi <juanpablo.lescano@gmail.com>
2014-11-13 16:10:11 -0300

    Corrección de signo: ahora si anda bien

--- volumenesFinitos/e1CentralDifference.m
+++ volumenesFinitos/e1CentralDifference.m
@@ -22,21 +22,21 @@ a = zeros(cantElementos,1);
 % phi1·(3k/h-v/2) + phi2(v/2-k/h) = 2/h*condBordeIzquierdoDirichlet +
 % velocidad·condBordeIzquierdoDirichlet + f·deltaX;
 
-K(1,1) = 3*coefConductividad/deltaX + velocidad/2;
+K(1,1) = -coefConductividad/deltaX + velocidad/2;
 K(1,2) = velocidad/2 - coefConductividad/deltaX;
-a(1)   = fuente*deltaX - velocidad*condBordeIzquierdoDirichlet+condBordeIzquierdoDirichlet*2*coefConductividad/deltaX;
+a(1)   = fuente*deltaX + velocidad*condBordeIzquierdoDirichlet + condBordeIzquierdoDirichlet*2*coefConductividad/deltaX;
 
 for j=2:cantElementos-1
-   K(j,j-1) = velocidad/2-coefConductividad/deltaX;
-   K(j,j)   = velocidad + 2 * coefConductividad / deltaX;
+   K(j,j-1) = -velocidad/2-coefConductividad/deltaX;
+   K(j,j)   = 2 * coefConductividad / deltaX;
    K(j,j+1) = velocidad/2-coefConductividad/deltaX;
 end
 
 a(2:cantElementos-1) = fuente*deltaX;
 
-K(cantElementos,cantElementos-1) =   velocidad/2 - coefConductividad/deltaX;
-K(cantElementos,cantElementos)   = 3*velocidad/2 + coefConductividad/deltaX;
-a(cantElementos)                 = fuente*deltaX + condBordeDerechoNeumann*(coefConductividad-velocidad*deltaX/2);
+K(cantElementos,cantElementos-1) = -velocidad/2 - coefConductividad/deltaX;
+K(cantElementos,cantElementos)   =  velocidad/2 + coefConductividad/deltaX;
+a(cantElementos)                 =  fuente*deltaX + condBordeDerechoNeumann*(coefConductividad-velocidad*deltaX/2);
 
 phi = K\a;
 phi = [condBordeIzquierdoDirichlet;phi];
